e54ab88abd73065e66e91cf889a74b0da9bd4b0e
Anastassia Slenduhhova <slenduhhova@gmail.com>
2022-12-21 22:02:38 +0200

    Week 4

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿
+string hello = "hello!";
+hello = hello.Replace(hello[0], Char.ToUpper( hello[0]));
+Console.WriteLine(hello);
+
+int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+Console.WriteLine(numbers.Length);
+return;
+Random rnd = new Random();
+int cpuRandom;
+
+bool LoopActive = true;
+
+
+while(LoopActive)
+{
+    cpuRandom = rnd.Next(1,4);
+    Console.WriteLine($"cpu has generated{cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+     
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        break;
+    }else
+    {
+        Console.WriteLine("oops. TRy again.");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
---
+++ week4loops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int i = 0
+
+for( int i = 0; i < 5; i++)
+{
+    Console.WriteLine("we don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("have a nice day!");
+//Console.WriteLine("we don't need no education");
